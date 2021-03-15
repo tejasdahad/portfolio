fd2fceeaf1a0607ce421c9892845be6448331cf1
Tejas Dahad <56474128+tejasdahad@users.noreply.github.com>
2021-03-15 12:16:23 +0530

    Done with logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import '../style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../Assets/logo.png';
+import logo from '../Assets/logo3.jpg';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
@@ -30,7 +30,9 @@ function NavBar() {
       className={navColour ? 'sticky' : 'navbar'}
     >
       <Container>
-        
+        <Navbar.Brand href='/'>
+          <img src={logo} className='img-fluid logo' alt='brand' />
+        </Navbar.Brand>
         <Navbar.Toggle
           aria-controls='responsive-navbar-nav'
           onClick={() => {
